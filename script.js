54983ed83ed36f7c35c6a842abde175ad4f9e451
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2024-01-03 16:39:22 +0530

    console playMode with Gameboard()

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function gameBoardFn(){
-    let a1, b1, c1, a2, b2, c2, a3, b3, c3;
-    let gameboardArr = [a1, b1, c1, a2, b2, c2, a3, b3, c3];
-    start(gameboardArr)
-    console.log(gameboardArr)
-}
+gameBoard = function(){
 
-function start(arr){
-    for ( let i = 0; i < arr.length ; i++){
-        arr[i] = false;
+gameBoardArr = [1,2,3,4,5,6,7,8,9]
+
+
+    function remove (value){
+        let index = gameBoardArr.indexOf(value);
+        if ( index > -1){
+            valueArr = gameBoardArr.splice(index , 1);
+            value = valueArr.pop();
+            return value;
+        }
+        else return;
     }
+
+
+return {gameBoardArr, remove};
+
 }
 
-gameBoardFn();
+let x = gameBoard().remove(9);
+let y = gameBoard().remove(10);
+
+console.log(x);
+console.log(x);
+console.log(gameBoardArr);
